196ee36917aaaa20e29656502b2802020d37e1f0
Eduard <algotech@Algotechs-MacBook-Pro.local>
2019-11-06 12:10:58 +0200

    Use knox auth and use email instead of username.

--- app/views/user.py
+++ app/views/user.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
@@ -9,6 +9,7 @@ from app.serializers.user import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
 
     @action(detail=False, methods=['get'])
     def all(self, request):
